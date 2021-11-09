53e2ca7580bbf42962e2c04a7d4773985495db1b
imzhangxian <imzhangxian@gmail.com>
2021-11-09 19:40:59 +0800

    Authorization before visiting contents

--- client/meter-app/src/App.js
+++ client/meter-app/src/App.js
@@ -1,11 +1,32 @@
+import { AuthContext } from "./context/AuthContext";
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
 import Navbar from "./components/Navbar"
 import Menubar from "./components/Menubar"
+import SignIn from "./components/SignIn";
 
 function App() {
+  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('user')));
   return (
     <div className="container text-sm mx-auto font-sans">
+    <AuthContext.Provider value={ { user, setUser } }>
       <Navbar />
-      <Menubar />
+      <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/" element=
+          {user ? 
+            <Menubar /> : <Navigate to='/signin' />
+          } />
+      </Routes>
+      </Router>
+    </AuthContext.Provider>
     </div>
   );
 }
